00aa5c27faab8fdeee0631b7a75cddac48b9cce9
Will D <will.dean0103@gmail.com>
2022-09-27 21:03:27 -0400

    asdf

--- public/ex.js
+++ public/ex.js
@@ -1,5 +1,3 @@
-const { response } = require('express');
-
 const newBtn = document.querySelector('newButton');
 const post = document.querySelector('newPost');
 const subBtn = document.querySelector('submit');
